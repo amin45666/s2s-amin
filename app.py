1040b3e15550edd039b16532942c937416821ede
Claudio Fantinuoli <61109621+fantinuoli@users.noreply.github.com>
2022-10-17 22:33:14 +0100

    Cleaning code

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ import os
 import json
 import requests, uuid
 import random
+import string
 from datetime import datetime
+from datamatrix import DataMatrix
+from flask_caching import Cache
 
 app = flask.Flask(__name__)
 app.secret_key = 'jhgwjhdgwjhd3d'
@@ -20,6 +23,19 @@ SEGMENTERAPI = 'https://asr-api.meetkudo.com' #web version
 
 sl = 'en'
 tls_list = ['es', 'it', 'he', 'ar', 'fr', 'pt']
+
+#This should become Session dependent
+dm = DataMatrix(length=10)
+dm.timeSL = 'undef'
+dm.durationSL = 'undef'
+dm.durationTL = 'undef'
+dm.wordsTL = 'undef'
+dm.delay = 'undef'
+print(dm)
+
+# Instantiate the cache
+cache = Cache()
+cache.init_app(app=app, config={"CACHE_TYPE": "simple", "CACHE_DEFAULT_TIMEOUT": 36000})
 
 
 ###############################################
@@ -46,41 +62,41 @@ def login():
             return redirect(url_for('login'))
     return render_template('login.html', title='Login')
 
+# this is the default SENDER view. Opening this page generates a session ID, initializes the Segmenter API with the session ID and creates the view
 @app.route("/sender", methods=["GET", "POST"])
 @roles_required("sender")
 def sender():
-    
-    print('OPENING SESSION')
-    url_client     = '' 
-    client         = 1
 
-    sessionId = random.randint(1000,9999)
+    # create a random session ID
+    sessionId = id_generator()
 
     #initiate a new session of API
     initialize_segmenterAPI(sessionId)
+    cache.set(sessionId, 0)
 
-    return render_template('sender.html', sessionId=sessionId, asr='asr', client=client, languages=tls_list, url_client=url_client)
+    return render_template('sender.html', sessionId=sessionId, languages=tls_list)
 
+# this is the advanced SENDER view (with testing options). Opening this page generates a session ID, initializes the Segmenter API with the session ID and creates the view
 @app.route("/consolle", methods=["GET", "POST"])
 @roles_required(["sender"])
 def consolle():
-    
-    print('OPENING SESSION')
-    url_client     = '' 
-    client         = 1
 
-    sessionId = random.randint(1000,9999)
+    # create a random session ID
+    sessionId = id_generator()
 
     #initiate a new session of API
     initialize_segmenterAPI(sessionId)
+    cache.set(sessionId, 0)
 
-    return render_template('consolle.html', sessionId=sessionId, asr='asr', client=client, languages=tls_list, url_client=url_client)
+    return render_template('consolle.html', sessionId=sessionId, languages=tls_list)
 
+# this is the receiver view which will receive the translation and the interpretation
 @app.route("/receiver", methods=["GET", "POST"])
 def receiver():
 
     return render_template("receiver.html", languages=tls_list)
 
+# this route returns info on the version log
 @app.route('/info')
 def info():
     changelog = [
@@ -106,7 +122,8 @@ def info():
         {'minor version': 19, 'details': 'new control of session initialisation for segmentation API', 'date': '2022-10-05'},
         {'minor version': 20, 'details': 'improved responsivness of RECEIVER', 'date': '2022-10-06'},
         {'minor version': 21, 'details': 'adding simple SENDER page with standard settings', 'date': '2022-10-12'},
-        {'minor version': 22, 'details': 'adding simple loggin page', 'date': '2022-10-13'},
+        {'minor version': 22, 'details': 'adding simple login page', 'date': '2022-10-13'},
+        {'minor version': 23, 'details': 'cleaning code', 'date': '2022-10-17'},
 
     ]
 
@@ -116,44 +133,90 @@ def info():
 # LIVE
 ###############################################
 
+#this is the orchestrator which is continuously called with the transcription and metadata
 @socketio.on("message")
-def handleMessage(data):
+def orchestrator(data):
 
-    asr = data['asr']
-    status = data['status']
-    room = data['room']
-    ck_lang = data['ck_lang']
-    paraphraseFeature = data['paraphraseFeature']
-    voiceSpeed = data['voiceSpeed']
+    #the orchestrator is receiving the transcription stream and a series of metadata associated to a session
+    asr               = data['asr']                 #transcription
+    status            = data['status']              #this is the status of ASR (temporary/final/silence)
+    room              = data['room']                #session ID
+    ck_lang           = data['ck_lang']             #use only 1 target language or many - should be changed with a list of required languages
+    paraphraseFeature = data['paraphraseFeature']   #use paraphrasing feature
+    voiceSpeed        = data['voiceSpeed']          #this will be later calculated by Orchestrator
 
-    print(f"\nI received from SENDER: '{asr}' with status '{status}' for Session '{room}'")
+    print(f"\nOrchestrator received from SENDER: '{asr}' with status '{status}' for Session '{room}'")
 
-    #send asr to segmenter and see if there is a response
+    # send asr to segmenter and see if there is a response
     mysegment = segment(asr, status, room)
-    paraphrasedAPPLIED = ''
+    
+    # recording timestamp when SL has been received
+    counterCALLBACK = cache.get(room)
+    dt = datetime.now()
+    ts = datetime.timestamp(dt)
+    dm.timeSL[counterCALLBACK] = ts
 
+    # recording duration SL as difference between this timestamp and previous one
+    if counterCALLBACK > 0:
+        SL_previous_timestamp = dm.timeSL[counterCALLBACK-1]
+        durationSL = (ts - SL_previous_timestamp) * 1000
+        dm.durationSL[counterCALLBACK] = durationSL
+
+    # proceed only if the Segmentator has returned a segment
     if mysegment:
         print("API returnes segment: " + mysegment)
 
-        #paraphrase segment 
+        paraphrasedAPPLIED = ''# this variable keeps track if the paraphrasing has been applied or not
+
+        # deciding if a segment needs to be paraphrased (typically to improve readibility and make it shorter)
         if paraphraseFeature:
             countOfWords = len(mysegment.split())
+            # we paraphrase only long segments for now. This parameter should be moved to Orchestrator default settings
             if countOfWords > 20:
                 mysegment = paraphrase(mysegment, sl)
                 print("Paraphrase returned: ")
                 print(mysegment)
                 paraphrasedAPPLIED = 'TRUE'
 
-        # translate only in default language. This should be handeld properly
+        # deciding in which languages to translate. For semplicity reasons, it is now either ES or ALL supported languages
+        # rewrite passing a list of languages
         tls_list_send = ['']
         if ck_lang != 'all':
             tls_list_send = ['es']
         else: 
             tls_list_send = tls_list
 
+        # translating the segment in target languages
         mytranslations = translate(mysegment, tls_list_send)
+
+        #estimate time of TTS based on language and speed of voice
+        #create session matrix with segment Nr + Duration SL + estimation in TL
+
         print("Translations returned: ")
         print(mytranslations)
+
+        #recording estimated duration of TL based on number of words
+        translation = mytranslations["en"] # this needs to be done for each TL that has been processed
+        word_list = translation.split()
+        number_of_words = len(word_list)
+        dm.wordsTL[counterCALLBACK] = number_of_words
+        #calculating duration
+        TTS_speed = 1 # set speed of TTS engine
+        language_dependend_duration_coefficient = {
+            'en' : 1,
+            'fr' : 1,
+            'it' : 1,
+            'de' : 1,
+            'es' : 1,
+            'pt' : 1
+        }
+
+        durationTL = number_of_words * language_dependend_duration_coefficient['en'] * TTS_speed * 1000 # expressed in milliseconds
+        dm.durationTL[counterCALLBACK] = durationTL
+        print(dm)
+        #updating number of callback for this session
+        counterCALLBACK=counterCALLBACK+1
+        cache.set(room, counterCALLBACK)
 
         mytranslations_json = json.dumps(mytranslations)
 
@@ -183,9 +246,10 @@ def on_left(data):
 
     emit("caption", f"User {user} left event {room},", room=room)
 
+#this inizialize a session of the Segmenter for this specific Session ID
 def initialize_segmenterAPI(sessionID):
 
-    #constructing parameters for call. tl should contain more languages
+    #we initialize the session with some parameters. The most important is the Session ID
     pload = {'lang': 'en', 'min_one_noun': True, 'remove_adv': False, 'delay': 3, 'sessionID': sessionID}
     endpoint = SEGMENTERAPI + '/startSession'
 
@@ -198,11 +262,12 @@ def initialize_segmenterAPI(sessionID):
         print("Error initializing of API: " + str(response))
         result = 'API initialisation error'
 
-def segment(text, status, room):
+# this calls the Segmenter
+def segment(text, status, sessionID):
     print("Calling Segmentation API")
 
     #constructing parameters for call. tl should contain more languages
-    pload = {'text': text, 'status': status, 'sessionID': room}
+    pload = {'text': text, 'status': status, 'sessionID': sessionID}
     endpoint = SEGMENTERAPI + '/parse'
 
     response = requests.post(url=endpoint, json=pload)
@@ -218,6 +283,7 @@ def segment(text, status, room):
         result = {"error": response}
         return result
 
+# this calls the translation API (now AZURE)
 def translate(text, tl_list):
     print("Calling Translation API")
 
@@ -226,7 +292,7 @@ def translate(text, tl_list):
     subscription_key = '830a4539e6914c8ea68e4b912ca678af'
     region = 'eastus'
 
-    #constructing parameters for call
+    #constructing language parameters for call
     sl = 'en'
     params = "&from=" + sl
     for tl in tl_list:
@@ -240,7 +306,7 @@ def translate(text, tl_list):
         'X-ClientTraceId': str(uuid.uuid4())
     }
 
-    # You can pass more than one object in body.
+    # You can pass more than one object in body
     body = [{
         'text' : text
     }]
@@ -258,6 +324,8 @@ def translate(text, tl_list):
 
     return translations
 
+# this calls the paraphrasing LM which is now a huggingface model hosted on their servers
+# we may switch to other models. We may want to host it ourselves
 def paraphrase(text, sl):
     print("Calling Paraphrasing API")
     API_URL = "https://api-inference.huggingface.co/models/prithivida/parrot_paraphraser_on_T5"
@@ -270,6 +338,7 @@ def paraphrase(text, sl):
 
     return paraphrased
 
+# simple hardcoded loging to avoid undesired eyes
 def login_authentication(email, password):
 
     #hardcoded login for quick check
@@ -285,6 +354,10 @@ def login_authentication(email, password):
             "role": "sender",
             }
         return response
+
+# this generates our session ID
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
