67cf69aecde16e71255080b826bd781638fcce55
Claudio Fantinuoli <61109621+fantinuoli@users.noreply.github.com>
2022-10-21 13:20:26 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ SL = "en"
 TLS_LIST = ["es", "it", "he", "zh-Hans", "fr", "pt"]
 VOICE_SPEED_DEFAULT = 15
 VOICE_STYLE = "General"
-USE_TIMING_MATRIX = True
+USE_TIMING_MATRIX = False
 SAMPLING_RATE = 3
 USE_REWRITING = False
 
